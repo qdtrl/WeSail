76a238d3cf38da58491d62d73ca525a2232665d0
qdtrl <quentindtouroul@gmail.com>
2023-11-27 14:50:10 +0100

    Initial Commit

---
+++ WeSail/WeSailApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSailApp.swift
+//  WeSail
+//
+//  Created by Quentin Dubut-Touroul on 27/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
