e50bf9426a87a188ef193569f1abaf6ee9bbc061
qdtrl <quentindtouroul@gmail.com>
2024-05-29 23:05:14 +0200

    Mise à jour de la vue d'ajout d'événement pour utiliser le style graphique pour les sélecteurs de date

--- WeSail/App/Boats/Show/AddBoatEvent.swift
+++ WeSail/App/Boats/Show/AddBoatEvent.swift
@@ -15,31 +15,33 @@ struct AddBoatEvent: View {
     @State var boat: Boat
 
     @State private var name: String = ""
-    @State private var startDate: Date = Date()
-    @State private var endDate: Date = Date()
+    @State private var startDate = Date()
+    @State private var endDate = Date()
 
     var body: some View {
         VStack {
             Form {
                 Section {
                     TextField("Nom de l'événement", text: $name)
-                    DatePicker("Date de début", selection: $startDate, displayedComponents: .date)
-                    DatePicker("Date de fin", selection: $endDate, displayedComponents: .date)
+
+                   DatePicker("Date de début", selection: $startDate, displayedComponents: [.date]).datePickerStyle(.graphical)
+                   DatePicker("Date de fin", selection: $endDate, displayedComponents: [.date]).datePickerStyle(.graphical)
+
                 }
                 Section {
                     Button("Créer l'événement") {
                        Task {
-                           let event = Event(
-                            id: UUID().uuidString,
-                            name: name,
-                            startDate: startDate,
-                            endDate: endDate,
-                            createdAt: Date(),
-                            participants: [])
-                                             
-                           try await boatsVM.addEventToBoat(boat, event)
-                             
-                           dismiss()
+                          let event = Event(
+                           id: UUID().uuidString,
+                           name: name,
+                           startDate: startDate,
+                           endDate: endDate,
+                           createdAt: Date(),
+                           participants: [])
+                                            
+                          try await boatsVM.addEventToBoat(boat, event)
+                            
+                          dismiss()
                        }
                     }
                 }
