1e05faeedca7c80c9739f0390000208a3868e2b4
qdtrl <quentindtouroul@gmail.com>
2024-09-13 17:28:34 +0200

    no run app

--- WeSailUITests/WeSailUITests.swift
+++ WeSailUITests/WeSailUITests.swift
@@ -24,8 +24,7 @@ final class WeSailUITests: XCTestCase {
 
     func testExample() throws {
         // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
+
 
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
@@ -33,9 +32,6 @@ final class WeSailUITests: XCTestCase {
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
             // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
         }
     }
 }
