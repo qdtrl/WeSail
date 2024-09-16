23c48bd8ba5ff771544111d58db518a3c8ee4843
qdtrl <quentindtouroul@gmail.com>
2024-09-16 13:10:49 +0200

    encore suppresion de ligne

--- WeSailUITests/WeSailUITests.swift
+++ WeSailUITests/WeSailUITests.swift
@@ -30,8 +30,6 @@ final class WeSailUITests: XCTestCase {
     }
 
     func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-        }
+        
     }
 }
